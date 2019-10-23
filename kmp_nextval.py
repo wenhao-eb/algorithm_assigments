02ce16a136d578a80a5ec14e40a4c18b1070f9bb
wenhao <1056009928@qq.com>
2019-10-23 17:31:43 +0800

    push files

---
+++ kmp_nextval.py
@@ -0,0 +1,34 @@
+def getNextVal(sub):
+    _next = [0] * len(sub)
+    _next[0] = -1
+    i = 0
+    j = -1
+    while i < len(sub) - 1:
+        if j == -1 or sub[i] == sub[j]:
+            i += 1
+            j += 1
+            if(sub[i]!=sub[j]):
+                _next[i]=j
+            else:
+                _next[i] = _next[j]
+        else:
+            j = _next[j]
+    return _next
+
+def KMP(txt, sub):
+
+
+    _next = getNextVal(sub)
+    i = 0
+    j = 0
+    while i < len(txt) and j < len(sub):
+        if j == -1 or txt[i] == sub[j]:
+            i += 1
+            j += 1
+        else:
+            j = _next[j]
+    if j == len(sub):
+        return i - j
+    return -1
+
+print(KMP('askldasdfasdfaghadfjf','jf'))
